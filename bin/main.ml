f10f93a2c380d4bd54cb7fe2e96437ca3a244886
Harris Bisset <RedstoneRebel399@gmail.com>
2024-10-31 11:23:47 +0000

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,34 +1,13 @@
-let invalid_args: _ =
-	fun () ->
-	begin
-		print_endline "invalid number of arguements given";
-		exit 1;
-	end;;
-	
-
-let starting_vals: int list = 
-	match Sys.argv with
-	| [|_;x;y|] -> 
-		if x < y then [int_of_string x; int_of_string y]
-		else invalid_args ();
-
-	| _ -> 
-		invalid_args ();;
-
-
-let if_mod (num: int) (modulo: int): bool =
-	(num mod modulo) = 0;;
-
-let if_mod_print (num: int) (modulo: int) (word: string) = 
-	if if_mod (num) (modulo) then print_string word;;
-
-
-for i = List.nth starting_vals 0 to List.nth starting_vals 1 do
-	if_mod_print (i) (3) ("fizz");
-	if_mod_print (i) (5) ("buzz");
-		
-	if if_mod (i) (3) || if_mod (i) (5) then 
-		print_endline "" 
-	else 
-		print_endline (string_of_int i);
-done;;
+let () =
+  let rec fbuzz n =
+    if n > 0
+    then (
+      match [ n mod 3; n mod 5 ] with
+      | [ 0; 0 ] -> print_endline "fizzbuzz"
+      | [ _; 0 ] -> print_endline "buzz"
+      | [ 0; _ ] -> print_endline "fizz"
+      | _ -> print_endline @@ string_of_int n);
+    fbuzz (n - 1)
+  in
+  fbuzz (int_of_string Sys.argv.(1))
+;;
